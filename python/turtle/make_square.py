3265cd56b4031ba9d98798ea2fa7ddfb426924ab
Jinnaph <111701121+Zoninanenia@users.noreply.github.com>
2022-09-22 01:00:03 +0700

    Update make_square.py

--- python/turtle/make_square.py
+++ python/turtle/make_square.py
@@ -1,13 +1,16 @@
-# turtle practicee
 import turtle
-
+# turtle practicee
 # draw square
 t = turtle.Turtle()
+#add color> t.color('pink')
 t.forward(100)
+#add color> t.color('blue')
 t.left(90)
 t.forward(100)
+#add color> t.color('green')
 t.left(90)
 t.forward(100)
+#add color> t.color('purple')
 t.left(90)
 t.forward(100)
 turtle.mainloop()
