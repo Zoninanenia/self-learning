397c23b3bc7ed0349aad69a320ef4af0ca21ca90
Jinnaph <111701121+Zoninanenia@users.noreply.github.com>
2022-09-21 23:00:12 +0700

    Create make_square.py

---
+++ python/turtle/make_square.py
@@ -0,0 +1,13 @@
+# turtle practicee
+import turtle
+
+# draw square
+t = turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+turtle.mainloop()
